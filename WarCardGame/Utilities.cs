e0634744300213462223eff5c2b5558670408d99
stim89 <bigsb1989@gmail.com>
2016-06-23 18:30:12 -0400

    utilities finshed and starting war card class

---
+++ WarCardGame/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WarCardGame
+{
+    static class Utilities
+    {
+        public static BitmapImage GetImage(string path)
+        {
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
+            img.EndInit();
+            return img;
+        }
+    }
+}

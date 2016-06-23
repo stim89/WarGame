1ab6a2826e65cbc95f2292f17210d91ab73ba473
stim89 <bigsb1989@gmail.com>
2016-06-23 17:03:54 -0400

    finshed main menu

--- WarCardGame/MainWindow.xaml.cs
+++ WarCardGame/MainWindow.xaml.cs
@@ -20,9 +20,71 @@ namespace WarCardGame
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        #region Construction
         public MainWindow()
         {
             InitializeComponent();
+
+            RegisterEvents();
         }
+
+        #endregion
+
+        #region Methods
+
+        private void RegisterEvents()
+        {
+            btn_newGame.MouseEnter += Btn_newGame_MouseEnter;
+            btn_rules.MouseEnter += Btn_rules_MouseEnter;
+            btn_exit.MouseEnter += Btn_exit_MouseEnter;
+
+            btn_newGame.MouseLeave += MainMenu_MouseLeave;
+            btn_rules.MouseLeave += MainMenu_MouseLeave;
+            btn_exit.MouseLeave += MainMenu_MouseLeave;
+        }
+
+        
+
+        #endregion
+
+        #region event handling
+        private void btn_newGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_rules_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void MainMenu_MouseLeave(object sender, MouseEventArgs e)
+        {
+            txt_info.Text = string.Empty;
+        }
+
+        private void Btn_exit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txt_info.Text = "exit your game";
+        }
+
+        private void Btn_rules_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txt_info.Text = "learn about the rules";
+        }
+
+        private void Btn_newGame_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txt_info.Text = "play game";
+        }
+
+
+        #endregion
     }
 }
